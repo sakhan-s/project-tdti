1a61e333557aa9ecd0576940966850899fbfe88f
sakhan-s <siradzhov@ya.ru>
2021-02-15 21:36:07 +0300

    feat: upload all files

---
+++ tdti/script.js
@@ -0,0 +1,12 @@
+window.onload = function preloader() {
+    setInterval(() => {
+        let preloader = document.getElementById('preloader');
+        preloader.style.display = 'none';
+    }, 2000);
+}
+window.addEventListener('scroll', () => {
+    document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
+}, false);
+AOS.init({
+    duration: 1200,
+})
