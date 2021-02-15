a51bfc8e351e448a3f84d4474df0572573019b71
sakhan-s <siradzhov@ya.ru>
2021-02-16 02:59:53 +0300

    feat: all works bad

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,7 +2,7 @@ window.onload = function preloader() {
     setInterval(() => {
         let preloader = document.getElementById('preloader');
         preloader.style.display = 'none';
-    }, 2000);
+    }, 500);
 }
 window.addEventListener('scroll', () => {
     document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
